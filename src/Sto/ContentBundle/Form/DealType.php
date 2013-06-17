baba0810e29eb3960b1641ffb5b9dcbb3870208f
Andrew M <amyshko@gmail.com>
2013-06-17 05:38:56 +0300

    Fix create deal

--- src/Sto/ContentBundle/Form/DealType.php
+++ src/Sto/ContentBundle/Form/DealType.php
@@ -12,23 +12,23 @@ class DealType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('name', 'text', [
-            'label' => 'Название акции',
-            'required' => true,
-            'attr' => [
-            'style' => 'width:100%',
-            ]
+            ->add('name', 'text', [
+                'label' => 'Название акции',
+                'required' => true,
+                'attr' => [
+                    'style' => 'width:100%',
+                ]
             ])
-        ->add('description', 'textarea', [
-            'label' => 'Описание',
-            'required' => false,
-            'render_optional_text' => false,
-            'attr' => [
-            'rows' => 4,
-            'style '=> 'width:100%'
-            ]
+            ->add('description', 'textarea', [
+                'label' => 'Описание',
+                'required' => false,
+                'render_optional_text' => false,
+                'attr' => [
+                'rows' => 4,
+                    'style '=> 'width:100%'
+                ]
             ])
-        ->add('autoServices', null, [
+            ->add('autoServices', null, [
                 'label' => 'Узлы и работы',
                 'required' => false,
                 'render_optional_text' => false,
@@ -45,126 +45,125 @@ class DealType extends AbstractType
                     'class' => 'select2'
                 ]
             ])
-        ->add('auto', null, [
-            'label' => 'Автомабили',
-            'required' => false,
-            'render_optional_text' => false,
-            'multiple' => true,
-            'class' => 'StoCoreBundle:Catalog\Base',
-            'query_builder' => function(EntityRepository $er) {
-                return $er->createQueryBuilder('catalog')
-                    ->where('catalog.parent is null')
-                ;
-            },
-            'attr' => [
-                'class' => 'select2'
-            ]
-        ])
-        ->add('image', 'file', [
-            'label' => 'Image',
-            'data_class' => 'Symfony\Component\HttpFoundation\File\File',
-            'property_path' => 'image',
-            'required' => false,
-            'label_render' => false,
-            'render_optional_text' => false,
-            'attr' => [
-            'data-image' => 'image1'
-            ]
+            ->add('auto', null, [
+                'label' => 'Автомабили',
+                'required' => false,
+                'render_optional_text' => false,
+                'multiple' => true,
+                'class' => 'StoCoreBundle:Catalog\Base',
+                'query_builder' => function(EntityRepository $er) {
+                    return $er->createQueryBuilder('catalog')
+                        ->where('catalog.parent is null')
+                    ;
+                },
+                'attr' => [
+                    'class' => 'select2'
+                ]
             ])
-        ->add('image2', 'file', [
-            'label' => 'Image2',
-            'data_class' => 'Symfony\Component\HttpFoundation\File\File',
-            'property_path' => 'image2',
-            'required' => false,
-            'label_render' => false,
-            'render_optional_text' => false,
-            'attr' => [
-            'data-image' => 'image2'
-            ]
+            ->add('image', 'file', [
+                'label' => 'Image',
+                'data_class' => 'Symfony\Component\HttpFoundation\File\File',
+                'property_path' => 'image',
+                'required' => false,
+                'label_render' => false,
+                'render_optional_text' => false,
+                'attr' => [
+                    'data-image' => 'image1'
+                ]
             ])
-        ->add('image3', 'file', [
-            'label' => 'Image3',
-            'data_class' => 'Symfony\Component\HttpFoundation\File\File',
-            'property_path' => 'image3',
-            'required' => false,
-            'label_render' => false,
-            'render_optional_text' => false,
-            'attr' => [
-            'data-image' => 'image3'
-            ]
+            ->add('image2', 'file', [
+                'label' => 'Image2',
+                'data_class' => 'Symfony\Component\HttpFoundation\File\File',
+                'property_path' => 'image2',
+                'required' => false,
+                'label_render' => false,
+                'render_optional_text' => false,
+                'attr' => [
+                    'data-image' => 'image2'
+                ]
             ])
-        ->add('terms', 'textarea', [
-            'label' => 'Условия участия',
-            'required' => false,
-            'render_optional_text' => false,
-            'attr' => [
-            'rows' => 4,
-            'style '=> 'width:100%'
-            ]
+            ->add('image3', 'file', [
+                'label' => 'Image3',
+                'data_class' => 'Symfony\Component\HttpFoundation\File\File',
+                'property_path' => 'image3',
+                'required' => false,
+                'label_render' => false,
+                'render_optional_text' => false,
+                'attr' => [
+                    'data-image' => 'image3'
+                ]
             ])
-        ->add('startDate', 'date', [
-            'widget' => 'single_text',
-            'datepicker' => true,
-            'label' => 'Начало',
-            'required' => true,
-            'attr' => [
-            'class' => 'input-small',
-            'style' => 'display: inline;'
-            ]
+            ->add('terms', 'textarea', [
+                'label' => 'Условия участия',
+                'required' => false,
+                'render_optional_text' => false,
+                'attr' => [
+                'rows' => 4,
+                    'style '=> 'width:100%'
+                ]
             ])
-        ->add('endDate', 'date', [
-            'widget' => 'single_text',
-            'datepicker' => true,
-            'label' => 'Конец',
-            'required' => true,
-            'attr' => [
-            'class' => 'input-small',
-            'style' => 'display: inline;'
-            ]
+            ->add('startDate', 'date', [
+                'widget' => 'single_text',
+                'datepicker' => true,
+                'label' => 'Начало',
+                'required' => true,
+                'attr' => [
+                'class' => 'input-small',
+                    'style' => 'display: inline;'
+                ]
             ])
-        ->add('startTime', 'time', [
-            'label' => 'Start time',
-            'required' => true,
-            'attr' => [
-            'style' => 'display: inline;'
-            ]
+            ->add('endDate', 'date', [
+                'widget' => 'single_text',
+                'datepicker' => true,
+                'label' => 'Конец',
+                'required' => true,
+                'attr' => [
+                'class' => 'input-small',
+                    'style' => 'display: inline;'
+                ]
             ])
-        ->add('endTime', 'time', [
-            'label' => 'End time',
-            'required' => true,
-            'attr' => [
-            'style' => 'display: inline;'
-            ]
+            ->add('startTime', 'time', [
+                'label' => 'Start time',
+                'required' => true,
+                'attr' => [
+                    'style' => 'display: inline;'
+                ]
             ])
-        ->add('place', null, [
-            'label' => 'Место проведе',
-            'required' => false,
-            'render_optional_text' => false,
-            'attr' => [
-            'class'=> 'input-xxlarge',
-            'onclick'=>"$('#myModal').modal();"
-            ]
+            ->add('endTime', 'time', [
+                'label' => 'End time',
+                'required' => true,
+                'attr' => [
+                    'style' => 'display: inline;'
+                ]
             ])
-        ->add('contactInformation', 'textarea', [
-            'label' => 'Контактное лицо',
-            'required' => false,
-            'render_optional_text' => false,
-            'attr' => [
-            'rows' => 2,
-            'style '=> 'width:100%'
-            ]
-
+            ->add('place', null, [
+                'label' => 'Место проведе',
+                'required' => false,
+                'render_optional_text' => false,
+                'attr' => [
+                'class'=> 'input-xxlarge',
+                    'onclick'=>"$('#myModal').modal();"
+                ]
             ])
-        ->add('type', null, [
-            'label' => 'Тип акции',
-            'required' => false,
-            'render_optional_text' => false,
-            'attr' => [
-            'class' => 'select2'
-            ]
+            ->add('contactInformation', 'textarea', [
+                'label' => 'Контактное лицо',
+                'required' => false,
+                'render_optional_text' => false,
+                'attr' => [
+                    'rows' => 2,
+                    'style '=> 'width:100%'
+                ]
             ])
-        ->add('gps', 'hidden', [])
-        ->add('companyId', 'hidden', [])
+            ->add('type', null, [
+                'label' => 'Тип акции',
+                'required' => false,
+                'render_optional_text' => false,
+                'attr' => [
+                    'class' => 'select2'
+                ]
+            ])
+            ->add('gps', 'hidden', [])
+            ->add('companyId', 'hidden', [])
         ;
     }
 
