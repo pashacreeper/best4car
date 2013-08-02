3a760e67668be9ecb8ea8c803d148a85e4e449b3
Roma Lapin <roman.lapin@gmail.com>
2013-08-02 18:09:45 +0400

    auto catalog sonata admin

---
+++ src/Sto/CoreBundle/Admin/Catalog/ModelAdmin.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Sto\CoreBundle\Admin\Catalog;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class ModelAdmin extends Admin
+{
+    protected $translationDomain = 'SonataAdmin';
+
+    public function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('name')
+            ->add('children')
+            ->add('parent')
+            ->add('uri')
+            ->add('visible')
+        ;
+    }
+
+    public function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', null, array(
+                'required' => true
+            ))
+            ->add('parent')
+            ->add('uri')
+            ->add('visible')
+        ;
+    }
+
+    public function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->addIdentifier('name')
+            ->add('parent')
+            ->add('uri')
+            ->add('visible')
+        ;
+    }
+
+    public function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('name')
+            ->add('parent')
+            ->add('uri')
+            ->add('visible')
+        ;
+    }
+}
---
+++ src/Sto/CoreBundle/Admin/Catalog/ModificationAdmin.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Sto\CoreBundle\Admin\Catalog;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class ModificationAdmin extends Admin
+{
+    protected $translationDomain = 'SonataAdmin';
+
+    public function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('name')
+            ->add('parent')
+            ->add('uri')    
+            ->add('visible')
+            ->add('numberOfDoors')
+            ->add('engine')
+            ->add('power')
+            ->add('fullSpeed')
+            ->add('bodyType')
+            ->add('startOfProduction')
+            ->add('closingOfProduction')
+        ;
+    }
+
+    public function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', null, array(
+                'required' => true
+            ))
+            ->add('parent')
+            ->add('uri')
+            ->add('visible')
+            ->add('numberOfDoors')
+            ->add('engine')
+            ->add('power')
+            ->add('fullSpeed')
+            ->add('bodyType')
+            ->add('startOfProduction')
+            ->add('closingOfProduction')
+        ;
+    }
+
+    public function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->addIdentifier('name')
+            ->add('parent')
+            ->add('uri')
+            ->add('visible')
+        ;
+    }
+
+    public function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('parent')
+            ->add('uri')
+            ->add('visible')
+            ->add('numberOfDoors')
+            ->add('engine')
+            ->add('power')
+            ->add('fullSpeed')
+            ->add('bodyType')
+            ->add('startOfProduction')
+            ->add('closingOfProduction')
+        ;
+    }
+}
