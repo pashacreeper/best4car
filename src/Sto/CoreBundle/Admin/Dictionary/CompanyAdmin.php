67a3701c4be3f9e4ea97f411ce6582bbfb6830e1
Roma Lapin <roman.lapin@gmail.com>
2013-08-05 11:49:41 +0400

    dictionaries admin

---
+++ src/Sto/CoreBundle/Admin/Dictionary/CompanyAdmin.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Sto\CoreBundle\Admin\Dictionary;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class CompanyAdmin extends Admin
+{
+    protected $translationDomain = 'SonataAdmin';
+
+    public function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('shortName')
+            ->add('name')
+            ->add('parent')
+            ->add('children')
+            ->add('position')
+            ->add('iconNameMap')
+            ->add('iconNameSmall')
+            ->add('iconNameLarge')
+            ->add('updatedAt')
+            ->add('autoServices')
+        ;
+    }
+
+    public function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('shortName')
+            ->add('name')
+            ->add('parent')
+            ->add('children')
+            ->add('position')
+            ->add('iconMap', 'file')
+            ->add('iconSmall', 'file')
+            ->add('iconLarge', 'file')
+            ->add('updatedAt')
+            ->add('autoServices')
+        ;
+    }
+
+    public function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->addIdentifier('shortName')
+            ->addIdentifier('name')
+            ->addIdentifier('parent')
+        ;
+    }
+
+    public function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('shortName')
+            ->add('name')
+            ->add('position')
+            ->add('parent')
+            ->add('iconNameMap')
+            ->add('iconNameSmall')
+            ->add('iconNameLarge')
+            ->add('updatedAt')
+            ->add('autoServices')
+        ;
+    }
+}
---
+++ src/Sto/CoreBundle/Admin/Dictionary/WorkAdmin.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Sto\CoreBundle\Admin\Dictionary;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class WorkAdmin extends Admin
+{
+    protected $translationDomain = 'SonataAdmin';
+
+    public function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('shortName')
+            ->add('name')
+            ->add('description')
+        ;
+    }
+
+    public function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('shortName')
+            ->add('name')
+            ->add('description')
+        ;
+    }
+
+    public function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->addIdentifier('name')
+            ->addIdentifier('shortName')
+            ->add('description')
+        ;
+    }
+
+    public function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('shortName')
+            ->add('name')
+            ->add('description')
+        ;
+    }
+}
