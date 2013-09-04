4718d0e5372b3b4dd43b01232d2bf70e18334084
Sergey Lunev <sergey@lunyov.ru>
2013-09-04 13:43:17 +0400

    Fix #226: city menu.

--- web/bootstrap/js/custom.city-choice.js
+++ web/bootstrap/js/custom.city-choice.js
@@ -1,8 +1,12 @@
+var selectedCity = '';
+
 var cityChoiser = function(defaultImage, Routing){
     $(document).ready(function(){
+        setCurrentCity(Routing)
+
         $('.townSelect').click(function() {
             $('.selectTownDropdown').toggle();
-        }); 
+        });
 
         $.getJSON(Routing.generate('api_city_selected'))
         .done(function (json) {
@@ -43,6 +47,13 @@ var cityChoiser = function(defaultImage, Routing){
     });
 };
 
+var setCurrentCity = function(Routing){
+    $.getJSON(Routing.generate('api_city_selected'))
+    .done(function (json) {
+        selectedCity = json.city;
+    });
+};
+
 var loadCityList = function(defaultImage, Routing){
     $('#country-select li > a').on("click", function(e) {
         e.preventDefault();
@@ -52,11 +63,18 @@ var loadCityList = function(defaultImage, Routing){
             var coun_area = $('#city-select ul');
             coun_area.empty();
             $.each(json, function (index, city) {
+                mark = '';
+                selectedClass = ''; 
+                if (city.name == selectedCity) {
+                    mark = '<span class="mark"></span>';
+                    selectedClass = 'activTown';
+                }
                 var img_src = (!city.icon_name) ? defaultImage : "/storage/images/countries/" + city.icon_name;
                 coun_area.append(
-                    '<li class="townSelectTabItem">'+
+                    '<li class="townSelectTabItem">' +
                     '<img src="' + img_src + '" style="width: 25px;height: 30px;">' +
-                    '<a href="#" class="city-link towns" data-city-id="' + city.id + '" data-city-name="' + city.name + '">' + city.name  + '</a></li>'
+                    '<a href="#" class="city-link towns '+selectedClass+'" data-city-id="' + city.id + '" data-city-name="' + city.name + '">' + city.name  + '</a>' + 
+                    mark + '</li>'
                 );
             });
 
