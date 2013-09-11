37c83c6e43a2d9de13937590b29895579ae0b0b8
Sergey Lunev <sergey@lunyov.ru>
2013-09-11 18:08:29 +0400

    CS fix.

--- src/Sto/CoreBundle/Admin/CompanySpecializationAdmin.php
+++ src/Sto/CoreBundle/Admin/CompanySpecializationAdmin.php
@@ -3,8 +3,6 @@ namespace Sto\CoreBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
 use Doctrine\ORM\EntityRepository;
 
