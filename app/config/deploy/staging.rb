16fac349057eede34f6815439993b6f345b65224
Sergeev Peter <sergeev.peter@gmail.com>
2013-09-23 17:06:36 +0400

    Deploy setup

---
+++ app/config/deploy/staging.rb
@@ -0,0 +1,7 @@
+set :domain,      "78.46.216.120"
+set :deploy_to,   "/var/www/php/#{application}"
+set :user,        "dev-user"
+
+role :web,        domain
+role :app,        domain, :primary => true
+role :db,         domain
