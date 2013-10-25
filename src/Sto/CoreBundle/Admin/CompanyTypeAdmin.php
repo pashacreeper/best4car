452f445742d2fdcebf3a1f7836847ca9ae761a82
Sergey Lunev <sergey@lunyov.ru>
2013-10-25 16:36:41 +0400

    Update #436: dont require services.

--- src/Sto/CoreBundle/Admin/CompanyTypeAdmin.php
+++ src/Sto/CoreBundle/Admin/CompanyTypeAdmin.php
@@ -63,6 +63,7 @@ class CompanyTypeAdmin extends Admin
                     'attr' => [
                         'class' => 'admin-service-select',
                     ],
+                    'required' => false
                 ])
             ->end()
         ;
