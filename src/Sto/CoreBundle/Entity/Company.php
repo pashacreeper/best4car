ca7a10572e6310d209bbd9c521ea52649eaeb271
Sergey Lunev <sergey@lunyov.ru>
2013-12-16 15:40:56 +0400

    Fix #592: load gif.

--- src/Sto/CoreBundle/Entity/Company.php
+++ src/Sto/CoreBundle/Entity/Company.php
@@ -77,7 +77,7 @@ class Company
     /**
      * @Assert\File(
      *     maxSize="5M",
-     *     mimeTypes={"image/png", "image/jpeg", "image/pjpeg"}
+     *     mimeTypes={"image/png", "image/jpeg", "image/pjpeg", "image/gif"}
      * )
      * @Vich\UploadableField(mapping="company_logo", fileNameProperty="logoName")
      */
