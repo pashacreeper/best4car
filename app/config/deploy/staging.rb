74b6a6f2af481efc0c7ae130f00ba8a7af4df212
Sergey Lunev <sergey@lunyov.ru>
2014-01-30 18:28:32 +0400

    Set staging branch

--- app/config/deploy/staging.rb
+++ app/config/deploy/staging.rb
@@ -6,6 +6,6 @@ role :web,        domain
 role :app,        domain, :primary => true
 role :db,         domain
 
-# set :branch,      "features"
+set :branch,      "email-notifications"
 
 after "deploy:update_code", "deploy:block_robots"
