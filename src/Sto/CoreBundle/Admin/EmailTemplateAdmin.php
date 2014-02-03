18579f3516ac34c0d00add6bd8181c674a3170b6
Sergey Lunev <sergey@lunyov.ru>
2014-02-03 13:51:44 +0400

    Update #601: email notifications help

--- src/Sto/CoreBundle/Admin/EmailTemplateAdmin.php
+++ src/Sto/CoreBundle/Admin/EmailTemplateAdmin.php
@@ -49,11 +49,65 @@ class EmailTemplateAdmin extends Admin
      */
     protected function configureFormFields(FormMapper $formMapper)
     {
+        $helpMessage = <<<HELP
+<table class="table table-condensed">
+    <thead>
+        <tr>
+            <td>Название переменной</td>
+            <td>Что делает</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="info">
+            <td colspan="2">Доступно во всех шаблонах</td>
+        </tr>
+        <tr>
+            <td><strong>%user%</strong></td><td>Имя пользователя</td>
+        </tr>
+        <tr>
+            <td><strong>%nick%</strong></td><td>Логин пользователя</td>
+        </tr>
+        <tr class="info">
+            <td colspan="2">Восстановление забытого пароля</td>
+        </tr>
+        <tr>
+            <td><strong>%link%</strong></td><td>Ссылка для восстановления пароля</td>
+        </tr>
+        <tr class="info">
+            <td colspan="2">Ответ на отзыв</td>
+        </tr>
+        <tr>
+            <td><strong>%link%</strong></td><td>Ссылка на страницу с отзывами, на которой был оставлен ответ на отзыв</td>
+        </tr>
+        <tr class="info">
+            <td colspan="2">Доступно во всех шаблонах для компаний</td>
+        </tr>
+        <tr>
+            <td><strong>%company%</strong></td><td>Название компании</td>
+        </tr>
+        <tr>
+            <td><strong>%link%</strong></td><td>Ссылка на страницу компании</td>
+        </tr>
+        <tr class="info">
+            <td colspan="2">Новый отзыв об акции</td>
+        </tr>
+        <tr>
+            <td><strong>%deal%</strong></td><td>Название предложения</td>
+        </tr>
+        <tr>
+            <td><strong>%link%</strong></td><td>ссылка на страницу предложения в раздел с отзывами</td>
+        </tr>
+    </tbody>
+</table>
+HELP;
+
         $formMapper
             ->add('title')
-            ->add('content')
             ->add('type', 'choice', [
                 'choice_list' => new EmailTemplateType()
+            ])
+            ->add('content', null, [
+                'help' => $helpMessage
             ])
         ;
     }
