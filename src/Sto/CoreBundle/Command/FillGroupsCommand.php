ef5bb804ceb3e925109b7a31a6dcdc4af63c366e
Igor Adrov <nucleartux@gmail.com>
2014-02-17 18:18:11 +0400

    Fix #645: add user to user group on registration

---
+++ src/Sto/CoreBundle/Command/FillGroupsCommand.php
@@ -0,0 +1,39 @@
+<?php
+namespace Sto\CoreBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Sto\CoreBundle\Entity\CompanyEmail;
+use Sto\UserBundle\Entity\Group;
+
+class FillGroupsCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('sto:fill:groups')
+            ->setDescription('Set user group where groups is empty');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+
+        $users = $em->getRepository('StoUserBundle:User')->findAll();
+
+        $userGroup = $em->getRepository('StoUserBundle:Group')->find(Group::USER);
+
+        $n = 0;
+        foreach ($users as $user) {
+            if (count($user->getGroups()) === 0) {
+                $n++;
+                $user->setGroups([$userGroup]);
+            }
+        }
+
+        $em->flush();
+
+        $output->writeln(sprintf("Updated users: %d", $n));
+    }
+}
