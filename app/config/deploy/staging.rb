d318c8b1331f0e340e3771df0a2a9211a463a995
Igor Adrov <nucleartux@gmail.com>
2014-02-25 20:49:44 +0400

    fix deploy

--- app/config/deploy/staging.rb
+++ app/config/deploy/staging.rb
@@ -6,6 +6,6 @@ role :web,        domain
 role :app,        domain, :primary => true
 role :db,         domain
 
-set :branch,      "optimize"
+set :branch,      "development"
 
 after "deploy:update_code", "deploy:block_robots"
