7f99cdf0af03595f6c9d3eb63bc3624451218114
Igor Adrov <nucleartux@gmail.com>
2014-03-14 16:58:05 +0400

    Revert "Fix #661: deals number link"
    This reverts commit d45ebacd385ab5487a4d4508ae3a2019e83e3a6a.

--- web/bootstrap/js/custom.js
+++ web/bootstrap/js/custom.js
@@ -531,11 +531,7 @@ var feedbackPage = function(){
 var profilePage = function(){
     (function(){
         $(window).hashchange( function(){
-            var hash = location.hash;
-            if (hash.indexOf('-')) {
-                hash = hash.substring(0, hash.indexOf('-'));
-            }
-            var $tabButton = $('.tabs ul.tabNavigation').find('a[href="' + hash + '"]');
+            var $tabButton = $('.tabs ul.tabNavigation').find('a[href="' + location.hash + '"]');
             if (!$tabButton.hasClass('selected')) {
                 $tabButton.click();
             }
@@ -549,9 +545,6 @@ var profilePage = function(){
         tabContainers.find('.content').hide();
         if (url.indexOf('#') + 1) {
             hash = url.substring(url.indexOf('#'));
-        }
-        if (hash && hash.indexOf('-')) {
-            hash = hash.substring(0, hash.indexOf('-'));
         }
 
         if (hash && hash.length > 1) {
